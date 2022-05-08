e9135b7e85afe345e1c2789c9545e727550e7f73
baeKH <bgh0611@naver.com>
2022-05-08 21:29:36 +0900

    화면 추가 및 jquery 추가

--- ApiProject/src/main/java/org/hdcd/controller/BoardController.java
+++ ApiProject/src/main/java/org/hdcd/controller/BoardController.java
@@ -10,10 +10,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/boards")
 public class BoardController {
 
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
@@ -45,6 +48,50 @@ public class BoardController {
 		boardList.add(board);
 		
 		ResponseEntity<List<Board>> entity = new ResponseEntity<>(boardList,HttpStatus.OK);
+		
+		return entity;
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<String> register() { 
+		logger.info("register");
+		
+		ResponseEntity<String> entity = new ResponseEntity<>("SUCCES", HttpStatus.OK);
+		
+		return entity;
+	}
+	
+	@RequestMapping(value = "/{boardNo}", method = RequestMethod.GET)
+	public ResponseEntity<Board> red(@PathVariable("boardNo") int boardNo) {
+		logger.info("read");
+		
+		Board board = new Board();
+		
+		board.setBardNo(1);
+		board.setTitle("향수");
+		board.setContent("넓은 별 동쪽 끝으로");
+		board.setWriter("hongkd");
+		board.setRegDate(new Date());
+		
+		ResponseEntity<Board> entity = new ResponseEntity<>(board, HttpStatus.OK);
+		
+		return entity;
+	}
+	
+	@RequestMapping(value = "/{boardNo}", method = RequestMethod.DELETE)
+	public ResponseEntity<String> remove(@PathVariable("boardNo") int boardNo) {
+		logger.info("remove");
+		
+		ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+		
+		return entity; 
+	}
+	
+	@RequestMapping(value = "/{baordNo}", method = RequestMethod.PUT)
+	public ResponseEntity<String> modify(@PathVariable("boardNo") int boardNo, @RequestBody Board board) {
+		logger.info("modify");
+		
+		ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.OK);
 		
 		return entity;
 	}
