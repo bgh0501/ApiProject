58d27ce17d01136ae4365e61ac9bd6aad93ff8d3
baeKH <bgh0611@naver.com>
2022-05-22 16:40:54 +0900

    swagger 연동수정

--- ApiProject/src/main/java/org/hdcdconfig/SwaggerConfig.java
+++ ApiProject/src/main/java/org/hdcdconfig/SwaggerConfig.java
@@ -4,6 +4,8 @@ package org.hdcdconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -16,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
 	
 	private ApiInfo metadata() {
@@ -33,9 +35,16 @@ public class SwaggerConfig {
 				.groupName("basic")
 				.select()
 				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.ant("/boards/**"))
+				.paths(PathSelectors.ant("/boards.*"))
 				.build()
 				.apiInfo(metadata());
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		super.addResourceHandlers(registry);
 	}
 	
 	
