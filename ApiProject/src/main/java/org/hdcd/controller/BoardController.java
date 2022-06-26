0baee71c40c784004cf89eb441d7a288f79aa204
bgh <bgh@naver.com>
2022-06-26 23:38:02 +0900

    교제 88 page 까

--- ApiProject/src/main/java/org/hdcd/controller/BoardController.java
+++ ApiProject/src/main/java/org/hdcd/controller/BoardController.java
@@ -15,12 +15,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.swagger.models.Response;
+
 @Controller
 @RequestMapping("/boards")
 public class BoardController {
 
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
 	//rpoduces 속성값에 "application/xml 미디어 타입을 지정한
-	@RequestMapping(value="{/boardNo}", method=RequestMethod.GET, produces="application/xml")
+	@RequestMapping(value="/{boardNo}", method=RequestMethod.GET, produces="application/xml")
 	public ResponseEntity<Board> readToXml(@PathVariable("boardNo") int boardNo) {
 		logger.info("readToXml");
 		
@@ -36,7 +40,39 @@ public class BoardController {
 		return entity;
 	}
 	
-	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	//produces 속성값에 "application/json" 미디어 타입을 지정한다.
+	@RequestMapping(value = "/{boardNo}", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Board> readToJson(@PathVariable("boardNo") int boardNo) {
+		logger.info("readToJson");
+		
+		Board board = new Board();
+		
+		board.setTitle("제목");
+		board.setContent("내용입니다.");
+		board.setWriter("홍길");
+		board.setRegDate(new Date());
+		
+		ResponseEntity<Board> entity = new ResponseEntity<Board>(board, HttpStatus.OK);
+		
+		return entity;
+	}
+	
+	//produces 속성값을 지정하지 않으면 기본값인 "application/json" 미디어 타입으로 지정된다
+	@RequestMapping(value = "/{boardNo}", method = RequestMethod.GET)
+	public ResponseEntity<Board> read(@PathVariable("boardNo") int boardNo) {
+		logger.info("read");
+		
+		Board board = new Board();
+		
+		board.setTitle("제목");
+		board.setContent("내용입니다.");
+		board.setWriter("홍길동");
+		board.setRegDate(new Date());
+		
+		ResponseEntity<Board> entity = new ResponseEntity<Board>(board, HttpStatus.OK);
+		
+		return entity;
+	}
 	
 	// /baords/ 경로에 GET 방식
 	@RequestMapping(value ="", method = RequestMethod.GET)
@@ -61,16 +97,16 @@ public class BoardController {
 		return entity;
 	}
 	
-	@RequestMapping(value = "/{boardNo}", method = RequestMethod.GET)
-	public ResponseEntity<Board> red(@PathVariable("boardNo") int boardNo) {
-		logger.info("read");
-		
-		Board board = new Board();
-		
-		ResponseEntity<Board> entity = new ResponseEntity<>(board, HttpStatus.OK);
-		
-		return entity;
-	}
+//	@RequestMapping(value = "/{boardNo}", method = RequestMethod.GET)
+//	public ResponseEntity<Board> red(@PathVariable("boardNo") int boardNo) {
+//		logger.info("read");
+//		
+//		Board board = new Board();
+//		
+//		ResponseEntity<Board> entity = new ResponseEntity<>(board, HttpStatus.OK);
+//		
+//		return entity;
+//	}
 	
 	@RequestMapping(value = "/{boardNo}", method = RequestMethod.DELETE)
 	public ResponseEntity<String> remove(@PathVariable("boardNo") int boardNo) {
@@ -91,7 +127,7 @@ public class BoardController {
 	}
 	
 	//consumes 속성값에 "applicaiotn/json" 미디어 타입을 지정한다.
-	@RequestMapping(value="/{boardNo}", method=RequestMethod.PUT, consumes="application/json")
+	@RequestMapping(value = "/{boardNo}", method=RequestMethod.PUT, consumes="application/json")
 	public ResponseEntity<String> modifyByJson(@PathVariable("boardNo") int boardNo, @RequestBody Board board) {
 		logger.info("modifyByJson");
 		
@@ -101,7 +137,7 @@ public class BoardController {
 	}
 	
 	//consumes 속성값에 "application/xml" 미디어 타입을 지정한다.
-	@RequestMapping(value="/{boardNo}", method=RequestMethod.PUT, consumes="application/xml")
+	@RequestMapping(value = "/{boardNo}", method=RequestMethod.PUT, consumes="application/xml")
 	public ResponseEntity<String> modifyByXml(@PathVariable("boardNo") int boardNo, @RequestBody Board board) {
 		logger.info("modifyBy XML boardNo" + boardNo);
 		
